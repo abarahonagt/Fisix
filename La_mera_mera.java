0b61789245287224d920a8b888f026829d75f397
Abner Noe Lopez Barahona <84617462+abarahonagt@users.noreply.github.com>
2023-01-02 21:34:39 -0600

    Add files via upload
    agregue mas elementos debido a que subi mal el repositorio

---
+++ La_mera_mera.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codex;
+
+
+import UI.Splash;
+
+/**
+ *
+ * @author Abner
+ */
+public class La_mera_mera {
+    
+    public static void main(String [] args){
+        Splash sp = new Splash();
+        sp.setVisible(true);
+        
+    }
+    
+}
